d554018e4c8f015e7205e84c72923c4cee70e63a
Teguhdl <teguhdianbangun@gmail.com>
2022-08-18 14:24:02 +0700

    dalate confirm,form edit,form add

--- formedit.php
+++ formedit.php
@@ -1,12 +1,14 @@
 <?php
+include './db/koneksi.php';
 
 session_start();
 
 if (!isset($_SESSION['username'])) {
-    header("Location: formlogin.php");
+    header("Location: index.php");
 }
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -136,6 +138,7 @@ if (!isset($_SESSION['username'])) {
             </a>
           </li>
         </ul>
+      </li>
 
 
           <li class="nav-item">
@@ -161,7 +164,7 @@ if (!isset($_SESSION['username'])) {
           <span>Data Denda</span>
         </a>
       </li>
-
+          </li>
           
 
 
@@ -170,130 +173,100 @@ if (!isset($_SESSION['username'])) {
     </ul>
 
   </aside><!-- End Sidebar-->
-
-  <main id="main" class="main">
+                                <!-- Form input data buku -->
+                                <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Dashboard Perpustakaan</h1>
+      <h1>Data Buku</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
+          <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+          <li class="breadcrumb-item">Data Buku</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
-
-    <section class="section dashboard">
+<section class="section">
       <div class="row">
-        <div class="col-lg-8">
-        <div class="row">
+        <div class="col-lg-12">
 
-        <!-- Customers Card -->
-            <div class="col-xxl-4 col-xl-12">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Ubah Data Buku</h5>
 
-              <div class="card info-card customers-card">
+                    <?php
 
-                <div class="card-body">
-                  <h5 class="card-title">Pengunjung <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-people"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>1244</h6>
-                      <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                    </div>
+                   
+                    $query = mysqli_query($conn, 
+                    "SELECT * FROM tb_buku WHERE id =
+                    '".$_GET["id"]."'");
+                    if ($data = mysqli_fetch_array($query))
+                    { 
+                        ?>
+                        
+              <!-- General Form Elements -->
+              <form action="databukuproses.php" method="post">
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">Id Buku</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" name ="id" value="<?php echo $data['id']; ?>" readonly>
                   </div>
-
                 </div>
-              </div>
-
-            </div><!-- End Customers Card -->
-
-               <!-- Customers Card -->
-            <div class="col-xxl-4 col-xl-12">
-
-              <div class="card info-card customers-card">
-
-                <div class="card-body">
-                  <h5 class="card-title">Peminjam <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-people"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>567</h6>
-                      <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                    </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">judul Buku</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" name ="judul_buku" value="<?php echo $data['judul_buku']; ?>" >
                   </div>
-
                 </div>
-              </div>
-
-            </div><!-- End Customers Card -->
-
-            <!-- Customers Card -->
-            <div class="col-xxl-4 col-xl-12">
-
-              <div class="card info-card customers-card">
-
-                <div class="card-body">
-                  <h5 class="card-title">Jumlah Buku <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="ri-book-2-fill"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>10492</h6>
-                      <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                    </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">jenis buku</label>
+                  <div class="col-sm-10">
+                    <select class="form-select" name="jenis_buku" value="<?php echo $data['jenis_buku']; ?>" >
+                                            <option selected>Pilih</option>
+                                            <option>Novel</option>
+                                            <option>Komik</option>
+                                            <option>Dongeng</option>
+                                            <option>Majalah</option>
+                                        </select>
                   </div>
-
                 </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">Pengarang</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" name ="pengarang" value="<?php echo $data['pengarang']; ?>" >
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">penerbit</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" name ="penerbit" value="<?php echo $data['penerbit']; ?>" >
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputText" class="col-sm-2 col-form-label">Tahun Terbit</label>
+                  <div class="col-sm-10">
+                    <input type="date" class="form-control" name ="thn" value="<?php echo $data['tahun_penerbit']; ?>" >
+                  </div>
+                </div>
+
+
+                     <button type="submit" class="btn btn-primary" name="edit">Ubah Data</button>
+                  </div>
+                </div>
+        
+              </form><!-- End General Form Elements -->
+                    <?php
+                    }
+                    ?>
               </div>
+          </div>
 
-            </div><!-- End Customers Card -->
-
-
+        </div>
+        </div>
+        
+      </div>
     </section>
-
-  </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
-    <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="template/assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="template/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="template/assets/vendor/chart.js/chart.min.js"></script>
-  <script src="template/assets/vendor/echarts/echarts.min.js"></script>
-  <script src="template/assets/vendor/quill/quill.min.js"></script>
-  <script src="template/assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="template/assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="template/assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="template/assets/js/main.js"></script>
-
+      <!-- Boostrap 5 -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
